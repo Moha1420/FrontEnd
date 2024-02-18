c70d74e38c46fa46c20ddaf028a1bee48c1924df
Your Name <you@example.com>
2024-02-18 22:27:26 +0300

    practice commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  
+function TemperatureConverter() {
+  const [celsius, setCelsius] = useState('');
+  const [fahrenheit, setFahrenheit] = useState('');
+
+  const handleCelsiusChange = (e) => {
+    const value = e.target.value;
+    setCelsius(value);
+    setFahrenheit((parseFloat(value) * 9 / 5) + 32 || '');
+  };
+
+  const handleFahrenheitChange = (e) => {
+    const value = e.target.value;
+    setFahrenheit(value);
+    setCelsius(((parseFloat(value) - 32) * 5 / 9) || '');
+  };
+
+  return (
+    <div>
+      <h2>Temperature Converter</h2>
+      <label>
+        Celsius:
+        <input
+          type="number"
+          value={celsius}
+          onChange={handleCelsiusChange}
+        /> 
+        <br /> <br /> 
+
+      </label>
+      <label>
+        Fahrenheit:
+        <input
+          type="number"
+          value={fahrenheit}
+          onChange={handleFahrenheitChange}
+        />
+      </label>
+    </div>
+  );
 }
 
-export default App;
+export default TemperatureConverter;
